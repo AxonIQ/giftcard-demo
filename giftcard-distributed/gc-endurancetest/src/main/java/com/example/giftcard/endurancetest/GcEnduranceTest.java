8ab1939719bfa382d027409a32faf909805259d9
Milan Savic <milan.savic@axoniq.io>
2018-03-27 16:12:25 +0200

    Refactoring. Limited number of exceptions to 1000.

--- giftcard-distributed/gc-endurancetest/src/main/java/com/example/giftcard/endurancetest/GcEnduranceTest.java
+++ giftcard-distributed/gc-endurancetest/src/main/java/com/example/giftcard/endurancetest/GcEnduranceTest.java
@@ -1,8 +1,8 @@
 package com.example.giftcard.endurancetest;
 
-import com.example.giftcard.api.CancelCmd;
 import com.example.giftcard.api.IssueCmd;
 import com.example.giftcard.api.RedeemCmd;
+import com.google.common.collect.Lists;
 import org.axonframework.commandhandling.CommandCallback;
 import org.axonframework.commandhandling.CommandMessage;
 import org.axonframework.commandhandling.gateway.CommandGateway;
@@ -11,8 +11,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.lang.invoke.MethodHandles;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Queue;
 import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -20,6 +22,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 import javax.annotation.PreDestroy;
 
 import static java.lang.String.format;
@@ -79,22 +82,19 @@ public class GcEnduranceTest {
         String id = UUID.randomUUID().toString();
         LOGGER.info("Executing test case #{} with id: {}.", enduranceTestInfo.testCaseStarted(), id);
 
-        try {
-            commandGateway.send(new IssueCmd(id, 100), new MonitorCommandCallback<>(enduranceTestInfo, () ->
-                    commandGateway.send(new RedeemCmd(id, 9), new MonitorCommandCallback<>(enduranceTestInfo, () ->
-                            commandGateway.send(new RedeemCmd(id, 9), new MonitorCommandCallback<>(enduranceTestInfo, () ->
-                                    commandGateway.send(new RedeemCmd(id, 9), new MonitorCommandCallback<>(enduranceTestInfo, () ->
-                                            commandGateway.send(new RedeemCmd(id, 9), new MonitorCommandCallback<>(enduranceTestInfo, () ->
-                                                    commandGateway.send(new RedeemCmd(id, 9), new MonitorCommandCallback<>(enduranceTestInfo, () ->
-                                                            commandGateway.send(new CancelCmd(id), new MonitorCommandCallback<>(enduranceTestInfo, null))))))))))))));
-        } catch (Exception e) {
-            LOGGER.error(format("Unexpected error occurred during processing test case with id: %s", id), e);
-            enduranceTestInfo.exception(e);
-        }
+        List<?> commands = Arrays.asList(new IssueCmd(id, 100), new RedeemCmd(id, 9));
 
-        scheduledExecutorService.schedule(() -> performTestCase(maxDelayInMillis),
-                                          rand(maxDelayInMillis),
-                                          TimeUnit.MILLISECONDS);
+        TestCase testCase = new TestCase(id,
+                                         commandGateway,
+                                         scheduledExecutorService,
+                                         maxDelayInMillis,
+                                         enduranceTestInfo,
+                                         () -> scheduledExecutorService
+                                                 .schedule(() -> performTestCase(maxDelayInMillis),
+                                                           rand(maxDelayInMillis),
+                                                           TimeUnit.MILLISECONDS),
+                                         commands);
+        testCase.process();
     }
 
     private int rand(int max) {
@@ -105,33 +105,82 @@ public class GcEnduranceTest {
         return Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors());
     }
 
+    private class TestCase {
+
+        private final String id;
+        private final Queue<?> commands;
+        private final CommandGateway commandGateway;
+        private final ScheduledExecutorService scheduledExecutorService;
+        private final int maxDelayInMillis;
+        private final EnduranceTestInfoImpl enduranceTestInfo;
+        private final Runnable onDone;
+
+        private TestCase(String id, CommandGateway commandGateway,
+                         ScheduledExecutorService scheduledExecutorService,
+                         int maxDelayInMillis,
+                         EnduranceTestInfoImpl enduranceTestInfo,
+                         Runnable onDone, Object... commands) {
+            this.id = id;
+            this.commandGateway = commandGateway;
+            this.scheduledExecutorService = scheduledExecutorService;
+            this.maxDelayInMillis = maxDelayInMillis;
+            this.enduranceTestInfo = enduranceTestInfo;
+            this.onDone = onDone;
+            this.commands = Lists.newLinkedList(Arrays.stream(commands).collect(Collectors.toList()));
+        }
+
+        private void process() {
+            if (commands.isEmpty()) {
+                onDone.run();
+                return;
+            }
+
+            Object command = commands.poll();
+            try {
+                commandGateway.send(command, new MonitorCommandCallback<>(enduranceTestInfo, () ->
+                        scheduledExecutorService
+                                .schedule(this::process, rand(maxDelayInMillis), TimeUnit.MILLISECONDS)));
+            } catch (Exception e) {
+                LOGGER.error(format("Unexpected error occurred during processing test case with id: %s", id), e);
+                enduranceTestInfo.exception(e);
+            }
+        }
+    }
+
     private class MonitorCommandCallback<T> implements CommandCallback<T, Object> {
 
         private final EnduranceTestInfoImpl enduranceTestInfo;
-        private final Runnable onSuccess;
+        private final Runnable nextStep;
 
-        private MonitorCommandCallback(EnduranceTestInfoImpl enduranceTestInfo, Runnable onSuccess) {
+        private MonitorCommandCallback(EnduranceTestInfoImpl enduranceTestInfo, Runnable nextStep) {
             this.enduranceTestInfo = enduranceTestInfo;
-            this.onSuccess = onSuccess;
+            this.nextStep = nextStep;
         }
 
         @Override
         public void onSuccess(CommandMessage<? extends T> commandMessage, Object result) {
             LOGGER.info("Command {} succeeded.", commandMessage.getCommandName());
             enduranceTestInfo.commandSucceeded();
-            if (onSuccess != null) {
-                onSuccess.run();
-            }
+            runNextStep();
         }
 
         @Override
         public void onFailure(CommandMessage<? extends T> commandMessage, Throwable cause) {
             LOGGER.warn(format("Command %s failed.", commandMessage.getCommandName()), cause);
             enduranceTestInfo.commandFailed(commandMessage, cause);
+            runNextStep();
+        }
+
+        private void runNextStep() {
+            if (nextStep != null) {
+                nextStep.run();
+            }
         }
     }
 
     private class EnduranceTestInfoImpl implements EnduranceTestInfo {
+
+        private static final int EXCEPTIONS_THRESHOLD = 1000;
 
         private final AtomicLong startedTestCases;
         private final AtomicLong successfulCommands;
@@ -151,6 +200,9 @@ public class GcEnduranceTest {
 
         private void commandFailed(CommandMessage<?> command, Throwable cause) {
             failedCommands.add(new FailedCommandInfo<>(command, cause));
+            if (failedCommands.size() + exceptions.size() > EXCEPTIONS_THRESHOLD) {
+                failedCommands.remove(0);
+            }
         }
 
         private void commandSucceeded() {
@@ -159,6 +211,9 @@ public class GcEnduranceTest {
 
         private void exception(Throwable t) {
             exceptions.add(t);
+            if (failedCommands.size() + exceptions.size() > EXCEPTIONS_THRESHOLD) {
+                exceptions.remove(0);
+            }
         }
 
         @Override
