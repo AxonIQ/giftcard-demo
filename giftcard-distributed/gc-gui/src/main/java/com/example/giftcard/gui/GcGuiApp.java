2e810a9e3d3696de133f60821ae7993dc61c9a43
Frans van Buul <frans.vanbuul@axoniq.io>
2018-04-25 21:47:55 +0200

    gui connecting to hub after startup

--- giftcard-distributed/gc-gui/src/main/java/com/example/giftcard/gui/GcGuiApp.java
+++ giftcard-distributed/gc-gui/src/main/java/com/example/giftcard/gui/GcGuiApp.java
@@ -1,13 +1,26 @@
 package com.example.giftcard.gui;
 
+import com.example.giftcard.api.CountCardSummariesQuery;
+import com.example.giftcard.api.CountCardSummariesResponse;
+import org.axonframework.queryhandling.QueryGateway;
+import org.axonframework.queryhandling.responsetypes.ResponseTypes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class GcGuiApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GcGuiApp.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void helloHub(ApplicationReadyEvent event) {
+		QueryGateway queryGateway = event.getApplicationContext().getBean(QueryGateway.class);
+		queryGateway.query(new CountCardSummariesQuery(),
+				ResponseTypes.instanceOf(CountCardSummariesResponse.class));
 	}
 
 }
