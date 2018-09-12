ca34720104b838593bd75c5a16e736bca334208b
Allard Buijze <allard.buijze@axoniq.io>
2018-09-13 00:09:22 +0200

    Removed unused imports

--- giftcard-distributed/gc-query/src/main/java/com/example/giftcard/query/GcQueryApp.java
+++ giftcard-distributed/gc-query/src/main/java/com/example/giftcard/query/GcQueryApp.java
@@ -1,17 +1,13 @@
 package com.example.giftcard.query;
 
-import org.axonframework.config.EventHandlingConfiguration;
-import org.axonframework.eventhandling.TrackingEventProcessorConfiguration;
-import org.axonframework.eventhandling.async.SequentialPerAggregatePolicy;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GcQueryApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GcQueryApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GcQueryApp.class, args);
+    }
 
 }
