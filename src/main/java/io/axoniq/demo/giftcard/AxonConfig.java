9c82ad10a7dd5398c2ccbb99693c60605302d495
Steven van Beelen <steven.vanbeelen@axoniq.io>
2022-11-10 10:51:40 +0100

    Adjust configuration of the LoggingInterceptor
    Adjust configuration of the LoggingInterceptor to use a ConfigurerModule
     Required due to Spring's auto wiring adjustments. Added some JavaDoc to
     clarify the decisions that are made.

--- src/main/java/io/axoniq/demo/giftcard/AxonConfig.java
+++ src/main/java/io/axoniq/demo/giftcard/AxonConfig.java
@@ -4,10 +4,11 @@ import com.thoughtworks.xstream.XStream;
 import org.axonframework.common.caching.Cache;
 import org.axonframework.common.caching.WeakReferenceCache;
 import org.axonframework.config.Configurer;
+import org.axonframework.config.ConfigurerModule;
 import org.axonframework.lifecycle.Phase;
 import org.axonframework.messaging.Message;
 import org.axonframework.messaging.interceptors.LoggingInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,27 +16,14 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 public class AxonConfig {
 
-    @Autowired
-    public void configureLoggingInterceptor(
-            @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") Configurer configurer
-    ) {
-        LoggingInterceptor<Message<?>> loggingInterceptor = new LoggingInterceptor<>();
+    @Bean
+    public LoggingInterceptor<Message<?>> loggingInterceptor() {
+        return new LoggingInterceptor<>();
+    }
 
-        // Registers the LoggingInterceptor on all infrastructure once they've been initialized by the Configurer:
-        configurer.onInitialize(config -> {
-            config.onStart(Phase.INSTRUCTION_COMPONENTS + 1, () -> {
-                config.commandBus().registerHandlerInterceptor(loggingInterceptor);
-                config.commandBus().registerDispatchInterceptor(loggingInterceptor);
-                config.eventBus().registerDispatchInterceptor(loggingInterceptor);
-                config.queryBus().registerHandlerInterceptor(loggingInterceptor);
-                config.queryBus().registerDispatchInterceptor(loggingInterceptor);
-                config.queryUpdateEmitter().registerDispatchInterceptor(loggingInterceptor);
-            });
-        });
-
-        // Registers a default Handler Interceptor for all Event Processors:
-        configurer.eventProcessing()
-                  .registerDefaultHandlerInterceptor((config, processorName) -> loggingInterceptor);
+    @Bean
+    public ConfigurerModule loggingInterceptorConfigurerModule(LoggingInterceptor<Message<?>> loggingInterceptor) {
+        return new LoggingInterceptorConfigurerModule(loggingInterceptor);
     }
 
     @Bean
@@ -50,5 +38,50 @@ public class AxonConfig {
         XStream xStream = new XStream();
         xStream.allowTypesByWildcard(new String[]{"io.axoniq.demo.giftcard.**"});
         return xStream;
+    }
+
+    /**
+     * An example {@link ConfigurerModule} implementation to attach configuration to Axon's configuration life cycle.
+     */
+    private static class LoggingInterceptorConfigurerModule implements ConfigurerModule {
+
+        private final LoggingInterceptor<Message<?>> loggingInterceptor;
+
+        private LoggingInterceptorConfigurerModule(LoggingInterceptor<Message<?>> loggingInterceptor) {
+            this.loggingInterceptor = loggingInterceptor;
+        }
+
+        @Override
+        public void configureModule(@NotNull Configurer configurer) {
+            configurer.eventProcessing(
+                              processingConfigurer -> processingConfigurer.registerDefaultHandlerInterceptor(
+                                      (config, processorName) -> loggingInterceptor
+                              )
+                      )
+                      .onInitialize(this::registerInterceptorForBusses);
+        }
+
+        /**
+         * Registers the {@link LoggingInterceptor} on the {@link org.axonframework.commandhandling.CommandBus},
+         * {@link com.google.common.eventbus.EventBus}, {@link org.axonframework.queryhandling.QueryBus}, and
+         * {@link org.axonframework.queryhandling.QueryUpdateEmitter}.
+         * <p>
+         * It does so right after the {@link Phase#INSTRUCTION_COMPONENTS}, to ensure all these infrastructure
+         * components are constructed.
+         *
+         * @param config The {@link org.axonframework.config.Configuration} to retrieve the infrastructure components
+         *               from.
+         */
+        @SuppressWarnings("resource") // We do not require to handle the returned Registration object.
+        private void registerInterceptorForBusses(org.axonframework.config.Configuration config) {
+            config.onStart(Phase.INSTRUCTION_COMPONENTS + 1, () -> {
+                config.commandBus().registerHandlerInterceptor(loggingInterceptor);
+                config.commandBus().registerDispatchInterceptor(loggingInterceptor);
+                config.eventBus().registerDispatchInterceptor(loggingInterceptor);
+                config.queryBus().registerHandlerInterceptor(loggingInterceptor);
+                config.queryBus().registerDispatchInterceptor(loggingInterceptor);
+                config.queryUpdateEmitter().registerDispatchInterceptor(loggingInterceptor);
+            });
+        }
     }
 }
